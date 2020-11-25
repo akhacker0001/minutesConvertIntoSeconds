65e7ae76651e55dcfd86354993194da5a50a7731
Ankit Kumar <ankitkumarasus@gmail.com>
2020-11-25 13:23:15 +0530

    minutesConvertIntoSenconds

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+var input1 = document.getElementById("input1");
+document.querySelector("button").addEventListener("click",function(){
+    var inputValue = input1.value;
+    var seconds = inputValue*60;
+    document.querySelectorAll("input")[1].value = seconds + "  seconds";
+})
+
+
+
+
+
+// function converter(){
+//     var a = b
+//     var
+
+// }
