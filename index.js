29fd8853b94d00ec5f0a7d3ab98cdf97494d8173
Ankit Kumar <ankitkumarasus@gmail.com>
2020-11-28 14:02:40 +0530

    minute converter

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 
 
 var input1 = document.getElementById("input1");
+var input2 = document.getElementById("input2");
 document.querySelector("button").addEventListener("click",function(){
     var inputValue = input1.value;
     var seconds = inputValue*60;
-    document.querySelectorAll("input")[1].value = seconds + "  seconds";
+    if (inputValue>0) {
+        input2.value = seconds;
+    }else{
+         var inputValue2 = input2.value;
+         var minutes = inputValue2/60;
+         input1.value = minutes.toFixed(2);
+    }
+  
+  
 })
 
 
